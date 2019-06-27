2fa35bafdbf8f940466e77766abde212959049cc
Lastro21 <ptokarev8@gmail.com>
2019-06-27 19:20:27 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping(value = "/")
+    public String showGreeting() {
+        return "Hello from usually WebApp!!!";
+    }
+
+}
